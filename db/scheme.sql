6ccfe2089674dca8e9b2f0619eb66ae89f18dbe3
Kionte Jones <jkionte20@gmail.com>
2020-11-23 19:01:55 -0500

    updated folders, added handlebars. more boiler-plate code.

---
+++ db/scheme.sql
@@ -0,0 +1,4 @@
+-- Drops the database if it exists currently --
+DROP DATABASE IF EXISTS project2_db;
+-- Creates the database --
+CREATE DATABASE project2_db;
---
+++ routes/api-routes.js
@@ -0,0 +1,5 @@
+const axios = require('axios')
+ 
+// GET REQUEST
+
+// POST REQUEST
